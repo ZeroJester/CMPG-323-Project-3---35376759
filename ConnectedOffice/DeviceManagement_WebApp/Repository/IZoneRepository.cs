2fe72c7189a8bf34100b3ff4ab11dc06a4c6e190
Marcel Joubert <85791779+ZeroJester@users.noreply.github.com>
2022-09-23 08:16:03 +0200

    Development (#1)
    * Initial commit to development branch
    
    * Added classes and interfaces
    
    * Implemented successful tier 2 endpoint (Zones)
    
    * Redo on all files
    
    Co-authored-by: MARCEL JOUBERT <35376759@student365.msfed.nwu.ac.za>

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IZoneRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeviceManagement_WebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IZoneRepository : IGenericRepository<Zone>
+    {
+        Zone GetMostRecentZone();
+    }
+
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,26 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(zone => zone.DateCreated).FirstOrDefault();
+        }
+
+    }
+
+
+}
