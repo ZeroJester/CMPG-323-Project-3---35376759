b24be474d7c43cdf7cbb0ee2e55f268854cdfa75
MARCEL JOUBERT <35376759@student365.msfed.nwu.ac.za>
2022-09-24 18:37:14 +0200

    Updated categories controller

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
@@ -24,11 +24,147 @@ namespace DeviceManagement_WebApp.Controllers
         //GET
 
         // GET: retrieve all items in a table format : Get All
-        //[Route("devices")]
         public IActionResult Index()
         {
             //return View(db.Products.ToList());
             return View(_cr.GetAll());
+        }
+
+        // GET: show one item in a singular item format : Get By ID
+        [Route("categories/{id}")]
+        [HttpGet]
+        public IActionResult Details(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category category = _cr.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(category);
+                Dispose();
+            }
+        }
+
+
+
+      
+
+        // DELETE: delete item base on item id given : Remove
+        [Route("category/delete/{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category category = _cr.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _cr.Remove(category);
+            return RedirectToAction("Index");
+        }
+
+     
+
+
+
+
+
+        //EDIT
+
+        /*// GET: retrieves a single item base on the item id given : Get By ID
+        public ActionResult Edit(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+                //return View(service);
+            }
+            Service service = _sr.GetById(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(service);
+                Dispose();
+            }
+        }
+
+        // POST: edits/updates the information of a single item matching the given id : Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(product).State = EntityState.Modified;
+                //db.SaveChanges();
+                _sr.AddRange(service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(service);
+                Dispose();
+            }
+        }
+
+        */
+
+
+
+
+
+
+        //CREATE
+
+        // GET: returns blank view : Get None
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: executes Add command, thus adding an item to the database : Add
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category != null)
+            {
+                _cr.Add(category);
+
+                //return CreatedAtAction("Index", service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Dispose();
+                return View(category);
+            }
+
+        }
+
+
+
+
+
+        //DISPOSE
+
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
         }
     }
 }
